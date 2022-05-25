a810e9d69720684c255ef858189dd1ee99161f66
ebmccray <eriennemccray@gmail.com>
2022-05-25 12:23:54 -0700

    DataVisualizer Bar Chart from sample data, library dependencies added to build.sbt

--- build.sbt
+++ build.sbt
@@ -9,3 +9,10 @@ lazy val root = (project in file("."))
 
 // https://mvnrepository.com/artifact/org.vegas-viz/vegas
 libraryDependencies += "org.vegas-viz" %% "vegas" % "0.3.11"
+libraryDependencies += "org.vegas-viz" %% "vegas-spark" % "0.3.11"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-core
+libraryDependencies += "org.apache.spark" %% "spark-core" % "2.4.6"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-sql
+libraryDependencies += "org.apache.spark" %% "spark-sql" % "2.4.6"
+// https://mvnrepository.com/artifact/org.apache.spark/spark-hive
+libraryDependencies += "org.apache.spark" %% "spark-hive" % "2.4.6"
--- src/main/scala/DataVisualizer.scala
+++ src/main/scala/DataVisualizer.scala
@@ -1,7 +1,36 @@
 import vegas._
+import vegas.sparkExt._
+import org.apache.spark.sql.{DataFrame, SparkSession}
 
 object DataVisualizer {
-  def main(args: Array[String]): Unit = {
+  val spark: SparkSession = SparkSession
+    .builder()
+    .appName("Spark-Vegas Data Visualizer")
+    .config("spark.master", "local")
+    .enableHiveSupport()
+    .getOrCreate()
 
+  val sample_data1: Seq[(String, Int)] = Seq(("stationery", 10), ("household products", 3), ("groceries", 5))
+  val sample_df1: DataFrame = spark.createDataFrame(sample_data1).toDF("Category Name", "Products Sold")
+
+  def main(args: Array[String]): Unit = {
+    val plot = Vegas("Chart Name")
+      .withDataFrame(sample_df1: DataFrame)
+      .encodeX("Category Name", Nom)
+      .encodeY("Products Sold", Quantitative)
+      .mark(Bar)
+
+    //plot.show
+    plotBarChart(sample_df1)
+  }
+
+  def plotBarChart(df: DataFrame): Unit = {
+    val plot = Vegas("")
+      .withDataFrame(df)
+      .encodeX("Category Name", Nom)
+      .encodeY("Products Sold", Quantitative)
+      .mark(Bar)
+
+    plot.show
   }
 }
