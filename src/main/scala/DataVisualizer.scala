b9cd05430faaa021d7d2dbc6b4431d4c67555b0a
ebmccray <eriennemccray@gmail.com>
2022-05-25 15:22:30 -0700

    Multiple-line line chart created.

--- src/main/scala/DataVisualizer.scala
+++ src/main/scala/DataVisualizer.scala
@@ -18,14 +18,16 @@ object DataVisualizer {
   val sample_data1: Seq[(String, Int)] = Seq(("stationery", 10), ("household products", 3), ("groceries", 5))
   val sample_data2: Seq[(String, Int)] = Seq(("00:00",50), ("01:00", 120), ("02:00",150), ("03:00",400))
   val sample_data3: Seq[(String, Int)] = Seq(("00:00",20), ("01:00", 100), ("02:00",350), ("03:00",470))
+  val sample_data4: Seq[(String, Int)] = Seq(("00:00",30), ("01:00", 110), ("02:00",250), ("03:00",450))
   val sample_df1: DataFrame = spark.createDataFrame(sample_data1).toDF("Category Name", "Products Sold")
   val sample_df2: DataFrame = spark.createDataFrame(sample_data2).toDF("Time", "Access")
   val sample_df3: DataFrame = spark.createDataFrame(sample_data3).toDF("Time", "Access")
+  val sample_df4: DataFrame = spark.createDataFrame(sample_data4).toDF("Time", "Access")
 
   def main(args: Array[String]): Unit = {
     //plotBarChart(sample_df1)
     //plotLineChart(sample_df2)
-    //plotMultiLineChart(Array(sample_df2, sample_df3))
+    plotMultiLineChart(Array(sample_df2, sample_df3, sample_df4))
   }
 
   def plotBarChart(df: DataFrame, chart_title: String = ""): Unit = {
@@ -73,16 +75,18 @@ object DataVisualizer {
   }
 
   def plotMultiLineChart(df_array: Array[DataFrame], chart_title: String = ""): Unit = {
+    val colors = Array("0653BE","BE06AF", "#BE7106", "06BE15", "36CCF0")
     val all_layers: ArrayBuffer[UnitSpecBuilder] = ArrayBuffer()
     for(df <- df_array) {
       val layer = createGraphLayer(df)
-      all_layers += layer
+      all_layers += layer.encodeColor(value = colors(df_array.indexOf(df)%colors.length))
     }
+
+    val layers_array: Array[UnitSpecBuilder] = all_layers.toArray
 
     val plot = Vegas.layered(chart_title)
       .withLayers(
-        all_layers(0),
-        all_layers(1)
+        layers_array: _*
       )
 
     plot.show
