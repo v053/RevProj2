7370a45b90d3aa3d711b1244853f4436ae6c3475
ebmccray <eriennemccray@gmail.com>
2022-05-25 14:33:17 -0700

    Visualizer takes spark DF; multi-line charts begun

--- src/main/scala/DataVisualizer.scala
+++ src/main/scala/DataVisualizer.scala
@@ -1,6 +1,11 @@
 import vegas._
 import vegas.sparkExt._
 import org.apache.spark.sql.{DataFrame, SparkSession}
+import org.apache.spark.sql.types._
+import vegas.DSL.UnitSpecBuilder
+
+import scala.collection.mutable.ArrayBuffer
+
 
 object DataVisualizer {
   val spark: SparkSession = SparkSession
@@ -11,26 +16,97 @@ object DataVisualizer {
     .getOrCreate()
 
   val sample_data1: Seq[(String, Int)] = Seq(("stationery", 10), ("household products", 3), ("groceries", 5))
+  val sample_data2: Seq[(String, Int)] = Seq(("00:00",50), ("01:00", 120), ("02:00",150), ("03:00",400))
+  val sample_data3: Seq[(String, Int)] = Seq(("00:00",20), ("01:00", 100), ("02:00",350), ("03:00",470))
   val sample_df1: DataFrame = spark.createDataFrame(sample_data1).toDF("Category Name", "Products Sold")
+  val sample_df2: DataFrame = spark.createDataFrame(sample_data2).toDF("Time", "Access")
+  val sample_df3: DataFrame = spark.createDataFrame(sample_data3).toDF("Time", "Access")
 
   def main(args: Array[String]): Unit = {
-    val plot = Vegas("Chart Name")
-      .withDataFrame(sample_df1: DataFrame)
-      .encodeX("Category Name", Nom)
-      .encodeY("Products Sold", Quantitative)
-      .mark(Bar)
-
-    //plot.show
-    plotBarChart(sample_df1)
+    //plotBarChart(sample_df1)
+    //plotLineChart(sample_df2)
+    //plotMultiLineChart(Array(sample_df2, sample_df3))
   }
 
-  def plotBarChart(df: DataFrame): Unit = {
-    val plot = Vegas("")
+  def plotBarChart(df: DataFrame, chart_title: String = ""): Unit = {
+    val col_name_array = df.columns
+    val data_type_array: ArrayBuffer[spec.Spec.Type] = ArrayBuffer()
+
+    for (column <- col_name_array) {
+      val my_data_type = df.schema(column).dataType
+      my_data_type match {
+        case StringType => data_type_array += Nom
+        case IntegerType => data_type_array += Quant
+        case _ => println("Not a recognized type")
+      }
+    }
+
+    val plot = Vegas(chart_title)
       .withDataFrame(df)
-      .encodeX("Category Name", Nom)
-      .encodeY("Products Sold", Quantitative)
+      .encodeX(col_name_array(0), data_type_array(0))
+      .encodeY(col_name_array(1), data_type_array(1))
       .mark(Bar)
 
     plot.show
+  }
+
+  def plotLineChart(df: DataFrame, chart_title: String = ""): Unit = {
+    val col_name_array = df.columns
+    val data_type_array: ArrayBuffer[spec.Spec.Type] = ArrayBuffer()
+
+    for (column <- col_name_array) {
+      val my_data_type = df.schema(column).dataType
+      my_data_type match {
+        case StringType => data_type_array += Nom
+        case IntegerType => data_type_array += Quant
+        case _ => println("Not a recognized type")
+      }
+    }
+
+    val plot = Vegas(chart_title)
+      .withDataFrame(df)
+      .encodeX(col_name_array(0), data_type_array(0))
+      .encodeY(col_name_array(1), data_type_array(1))
+      .mark(Line)
+
+    plot.show
+  }
+
+  def plotMultiLineChart(df_array: Array[DataFrame], chart_title: String = ""): Unit = {
+    val all_layers: ArrayBuffer[UnitSpecBuilder] = ArrayBuffer()
+    for(df <- df_array) {
+      val layer = createGraphLayer(df)
+      all_layers += layer
+    }
+
+    val plot = Vegas.layered(chart_title)
+      .withLayers(
+        all_layers(0),
+        all_layers(1)
+      )
+
+    plot.show
+  }
+
+  def createGraphLayer(df: DataFrame): UnitSpecBuilder = {
+    val col_name_array = df.columns
+    val data_type_array: ArrayBuffer[spec.Spec.Type] = ArrayBuffer()
+
+    for (column <- col_name_array) {
+      val my_data_type = df.schema(column).dataType
+      my_data_type match {
+        case StringType => data_type_array += Nom
+        case IntegerType => data_type_array += Quant
+        case _ => println("Not a recognized type")
+      }
+    }
+
+    val my_layer = Layer()
+      .withDataFrame(df)
+      .encodeX(col_name_array(0), data_type_array(0))
+      .encodeY(col_name_array(1), data_type_array(1))
+      .mark(Line)
+
+    my_layer
   }
 }
