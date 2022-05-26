b6a8165643327b64ec6499b24793457a6a2dc190
ebmccray <eriennemccray@gmail.com>
2022-05-26 15:31:49 -0700

    Tried to figure out query issues in Question 2

--- src/main/scala/DataVisualizer.scala
+++ src/main/scala/DataVisualizer.scala
@@ -22,12 +22,7 @@ object DataVisualizer {
   /*val df = spark.read.csv("data.csv")
   df.createOrReplaceTempView("Orders")
   df.registerTempTable("Orders")
-  val dfQ2_1 = spark.sql ("SELECT _c9,_c4, sum(_c7) FROM Orders where _c9 like '2021%' AND _c4 = 'Cutting board'  group by _c9").toDF("Product","Time","Products Sold")
-  dfQ2_1.show()
-  val dfQ1_1 = spark.sql("SELECT _c5, sum(_c7) FROM Orders group by _c5").toDF("Category","Products Sold")
-  //dfQ1_1.show()
-  val dfQ1_2 = spark.sql("SELECT _c11, _c5, sum(_c7) FROM Orders group by _c11, _c5").toDF("Country","Category","Products Sold")
-  //dfQ1_2.show()*/
+  */
 
   // Sample Data
   val sample_data1: Seq[(String, Int)] = Seq(("stationery", 10), ("household products", 3), ("groceries", 5))
@@ -42,7 +37,9 @@ object DataVisualizer {
   //val df = spark.read.csv("data.csv")
   val df: DataFrame = spark.read.format("csv")
     .option("header","false")
+    .option("inferSchema", "true")
     .load("C:\\Users\\Erienne Work\\Documents\\Revature\\Training Projects\\Project2\\data.csv")
+    //.load("data.csv")
     .toDF("order_id","customer_id","customer_name",
       "product_id","product_name", "product_category","payment_type","qty","product_price",
       "datetime","country","city",
@@ -50,30 +47,34 @@ object DataVisualizer {
 
   df.createOrReplaceTempView("orders")
 
-  /*df.createOrReplaceTempView("Orders")
-  df.registerTempTable("Orders")
-  val sqlDF = spark.sql ("SELECT * FROM Orders where _c6 = 'Wallet'")
-  sqlDF.show()*/
-
   def main(args: Array[String]): Unit = {
-    //plotBarChart(sample_df1)
-    //plotLineChart(sample_df2)
-    //plotMultiLineChart(Array(sample_df2, sample_df3, sample_df4))
-    queryTopSellingProductByCountry()
+    //queryYearPopularity("'Computer case'")
+    //queryTopSellingProductByCountry()
+    //queryTopSellingProduct()
+    //queryYearPopularity("'Computer case'")
   }
 
 
   // Queries
   def queryTopSellingProduct(): Unit = {
-    val query_selection_df: DataFrame = spark.sql("SELECT product_category, COUNT(order_id) AS quantity FROM orders WHERE product_category != 'null' AND country != 'null' GROUP BY product_category")
-    plotBarChart(query_selection_df)
+    val query_selection_df: DataFrame = spark.sql("SELECT product_category, SUM(qty) AS quantity FROM orders WHERE product_category != 'null' AND country != 'null' GROUP BY product_category")
+    plotBarChart(query_selection_df, chart_title = "Top Selling Product Categories")
   }
 
   def queryTopSellingProductByCountry(): Unit = {
-    val query_selection_df: DataFrame = spark.sql("SELECT country, COUNT(order_id) AS quantity, product_category FROM orders WHERE product_category != 'null' AND country != 'null' GROUP BY country, product_category")
-    plotBarChart(query_selection_df, has_categories = true, category_filter = "product_category")
+    val query_selection_df: DataFrame = spark.sql("SELECT country, SUM(qty) AS quantity, product_category FROM orders WHERE product_category != 'null' AND country != 'null' GROUP BY country, product_category")
+    plotBarChart(query_selection_df, has_categories = true, category_filter = "product_category", chart_title = "Top Selling Product Category By Country")
   }
 
+  def queryYearPopularity(product_name: String, year: String = "2021"): Unit = {
+    val dfQ2_1 = spark.sql (s"SELECT datetime, SUM(qty) FROM orders where product_name = $product_name AND qty != 'null'")
+    plotLineChart(dfQ2_1)
+    //dfQ2_1.show()
+    //val dfQ1_1 = spark.sql("SELECT _c5, sum(_c7) FROM Orders group by _c5").toDF("Category","Products Sold")
+    //dfQ1_1.show()
+    //val dfQ1_2 = spark.sql("SELECT _c11, _c5, sum(_c7) FROM Orders group by _c11, _c5").toDF("Country","Category","Products Sold")
+    //dfQ1_2.show()
+  }
 
   // Plotting
   def getPlotInfo(df: DataFrame): ArrayBuffer[spec.Spec.Type] = {
@@ -82,6 +83,7 @@ object DataVisualizer {
 
     for (column <- col_name_array) {
       val my_data_type = df.schema(column).dataType
+      println(column + ", " + my_data_type)
       my_data_type match {
         case StringType => data_type_array += Nom
         case IntegerType => data_type_array += Quant
@@ -120,17 +122,28 @@ object DataVisualizer {
     }
   }
 
-  def plotLineChart(df: DataFrame, chart_title: String = ""): Unit = {
+  def plotLineChart(df: DataFrame, has_categories: Boolean = false, category_filter: String = "", chart_title: String = ""): Unit = {
     val col_name_array = df.columns
     val data_type_array = getPlotInfo(df)
 
-    val plot = Vegas(chart_title)
-      .withDataFrame(df)
-      .encodeX(col_name_array(0), data_type_array(0))
-      .encodeY(col_name_array(1), data_type_array(1))
-      .mark(Line)
+    if (has_categories) {
+      val plot = Vegas(chart_title)
+        .withDataFrame(df)
+        .encodeX(col_name_array(0), data_type_array(0))
+        .encodeY(col_name_array(1), data_type_array(1))
+        .mark(Line)
 
-    plot.show
+      plot.show
+    } else {
+      val plot = Vegas(chart_title)
+        .withDataFrame(df)
+        .encodeX(col_name_array(0), data_type_array(0))
+        .encodeY(col_name_array(1), data_type_array(1))
+        .mark(Line)
+        .encodeColor(field=category_filter, dataType=Nominal)
+
+      plot.show
+    }
   }
 
   def plotMultiLineChart(df_array: Array[DataFrame], chart_title: String = ""): Unit = {
