04d5c4e5ab326dc135f35b1aa66aa8ee71c8931b
GeorgeCobb <70028860+GeorgeCobb@users.noreply.github.com>
2022-05-26 15:14:27 -0500

    Testing Q1 and Q2 sqls are there

--- src/main/scala/DataVisualizer.scala
+++ src/main/scala/DataVisualizer.scala
@@ -1,10 +1,12 @@
+
+import Generator.getClass
 import vegas._
 import vegas.sparkExt._
-import vegas.DSL.UnitSpecBuilder
-
 import org.apache.spark.sql.{DataFrame, SparkSession}
 import org.apache.spark.sql.types._
+import vegas.DSL.UnitSpecBuilder
 
+import java.io.File
 import scala.collection.mutable.ArrayBuffer
 
 
@@ -16,6 +18,16 @@ object DataVisualizer {
     .enableHiveSupport()
     .getOrCreate()
 
+  val df = spark.read.csv("data.csv")
+  df.createOrReplaceTempView("Orders")
+  df.registerTempTable("Orders")
+  val dfQ2_1 = spark.sql ("SELECT _c9,_c4, sum(_c7) FROM Orders where _c9 like '2021%' AND _c4 = 'Cutting board'  group by _c9").toDF("Product","Time","Products Sold")
+  dfQ2_1.show()
+  val dfQ1_1 = spark.sql("SELECT _c5, sum(_c7) FROM Orders group by _c5").toDF("Category","Products Sold")
+  //dfQ1_1.show()
+  val dfQ1_2 = spark.sql("SELECT _c11, _c5, sum(_c7) FROM Orders group by _c11, _c5").toDF("Country","Category","Products Sold")
+  //dfQ1_2.show()
+
   val sample_data1: Seq[(String, Int)] = Seq(("stationery", 10), ("household products", 3), ("groceries", 5))
   val sample_data2: Seq[(String, Int)] = Seq(("00:00",50), ("01:00", 120), ("02:00",150), ("03:00",400))
   val sample_data3: Seq[(String, Int)] = Seq(("00:00",20), ("01:00", 100), ("02:00",350), ("03:00",470))
@@ -23,12 +35,12 @@ object DataVisualizer {
   val sample_df1: DataFrame = spark.createDataFrame(sample_data1).toDF("Category Name", "Products Sold")
   val sample_df2: DataFrame = spark.createDataFrame(sample_data2).toDF("Time", "Access")
   val sample_df3: DataFrame = spark.createDataFrame(sample_data3).toDF("Time", "Access")
-  val sample_df4: DataFrame = spark.createDataFrame(sample_data4).toDF("Time", "Access")
+
 
   def main(args: Array[String]): Unit = {
-    //plotBarChart(sample_df1)
-    //plotLineChart(sample_df2)
-    plotMultiLineChart(Array(sample_df2, sample_df3, sample_df4))
+    //plotBarChart(dfQ1_2)
+    //plotLineChart(dfQ2_1)
+    plotMultiLineChart(Array(dfQ2_1))
   }
 
   def plotBarChart(df: DataFrame, chart_title: String = ""): Unit = {
@@ -40,6 +52,7 @@ object DataVisualizer {
       my_data_type match {
         case StringType => data_type_array += Nom
         case IntegerType => data_type_array += Quant
+        case DoubleType => data_type_array += Quant
         case _ => println("Not a recognized type")
       }
     }
@@ -62,6 +75,7 @@ object DataVisualizer {
       my_data_type match {
         case StringType => data_type_array += Nom
         case IntegerType => data_type_array += Quant
+        case DoubleType => data_type_array += Quant
         case _ => println("Not a recognized type")
       }
     }
@@ -102,6 +116,7 @@ object DataVisualizer {
       my_data_type match {
         case StringType => data_type_array += Nom
         case IntegerType => data_type_array += Quant
+        case DoubleType => data_type_array += Quant
         case _ => println("Not a recognized type")
       }
     }
@@ -109,7 +124,7 @@ object DataVisualizer {
     val my_layer = Layer()
       .withDataFrame(df)
       .encodeX(col_name_array(0), data_type_array(0))
-      .encodeY(col_name_array(1), data_type_array(1))
+      .encodeY(col_name_array(2), data_type_array(2))
       .mark(Line)
 
     my_layer
